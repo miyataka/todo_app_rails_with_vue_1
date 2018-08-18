5de7bc0d651b61faf200d266ef513332c500460a
Takayuki Miyahara <voyager.3taka28@gmail.com>
2018-08-18 13:56:04 +0900

    make seeds, CSRF token auth off for curl testing

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,4 +60,8 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # temporary config by taka
+  # Disable request forgery protection in develop environment.
+  config.action_controller.allow_forgery_protection = false
 end
